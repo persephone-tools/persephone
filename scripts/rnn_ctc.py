fd6586cb9cb1c94f026057d5bb5c7f2f75165a21
Oliver Adams <oliver.adams@gmail.com>
2017-02-21 11:52:06 +1100

    RNN fitting one training example.

--- scripts/rnn_ctc.py
+++ scripts/rnn_ctc.py
@@ -3,34 +3,20 @@ import timit
 import numpy as np
 import tensorflow as tf
 
-def SimpleSparseTensorFrom(x):
-    """Create a very simple SparseTensor with dimensions (batch, time).
-    Args:
-        x: a list of lists of type int
-    Returns:
-        x_ix and x_val, the indices and values of the SparseTensor<2>.
-
-    Note: this code was taken from
-    tensorflow/python/kernel_tests/ctc_loss_op_test.py. It's purpose is to do
-    magic to a batch of label sequences so it can be fed to tf.nn.ctc_loss().
-    """
-
-    from tensorflow.python.framework import constant_op
-    from tensorflow.python.framework import dtypes
-    from tensorflow.python.framework import sparse_tensor
-
-    x_ix = []
-    x_val = []
-    for batch_i, batch in enumerate(x):
-        for time, val in enumerate(batch):
-            x_ix.append([batch_i, time])
-            x_val.append(val)
-    x_shape = [len(x), np.asarray(x_ix).max(0)[1] + 1]
-    x_ix = constant_op.constant(x_ix, dtypes.int64)
-    x_val = constant_op.constant(x_val, dtypes.int32)
-    x_shape = constant_op.constant(x_shape, dtypes.int64)
-
-    return sparse_tensor.SparseTensor(x_ix, x_val, x_shape)
+def target_list_to_sparse_tensor(targetList):
+    '''make tensorflow SparseTensor from list of targets, with each element
+       in the list being a list or array with the values of the target sequence
+       (e.g., the integer values of a character map for an ASR target string)
+       See https://github.com/tensorflow/tensorflow/blob/master/tensorflow/contrib/ctc/ctc_loss_op_test.py
+       for example of SparseTensor format'''
+    indices = []
+    vals = []
+    for tI, target in enumerate(targetList):
+        for seqI, val in enumerate(target):
+            indices.append([tI, seqI])
+            vals.append(val)
+    shape = [len(targetList), np.asarray(indices).max(0)[1]+1]
+    return (np.array(indices), np.array(vals), np.array(shape))
 
 def collapsed_timit(batch, batch_size=100):
     """ Converts timit into an array of format (batch_size, freq, time). Except
@@ -102,21 +88,24 @@ def create_graph(batch_size=100, utter_len=778, freqfeats=78,
 
     # Should engineer it so that I don't have to pick and choose ops and
     # tensors to return.
-    return feats, optimizer, ler, decoded
+    return feats, targets, seq_len, optimizer, ler, decoded
 
-if __name__ == "__main__":
-    batch_size=100
-    feats, optimizer, ler, decoded = create_graph(batch_size=batch_size)
+def train():
+    batch_size=1
+    feats, targets, seq_len, optimizer, ler, decoded = create_graph(batch_size=batch_size)
 
     init = tf.global_variables_initializer()
     batch_gen = timit.batch_gen(batch_size=batch_size, labels="phonemes")
     train_x, train_y = collapsed_timit(next(batch_gen))
-    seq_lens = np.asarray([len(s) for s in train_y], dtype=np.int64)
-    train_y = SimpleSparseTensorFrom(train_y)
+    seq_lens = np.asarray([len(s) for s in train_y], dtype=np.int32)
+    train_y = target_list_to_sparse_tensor(train_y)
     print(seq_lens)
     print(train_y)
 
     with tf.Session() as sess:
+        sess.run(init)
         for _ in range(1000):
-            print(sess.run([optimizer, ler, decoded], feed_dict={feats: train_x,
-                    targets: train_y, seq_len: seq_lens}).shape)
+            print(sess.run([optimizer, ler, decoded], feed_dict={feats: train_x, targets: train_y, seq_len: seq_lens}))
+
+if __name__ == "__main__":
+    train()
