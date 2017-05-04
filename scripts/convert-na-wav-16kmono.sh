111a3a8b038bc135054de3b42034a6b809375e9e
Oliver Adams <oliver.adams@gmail.com>
2017-05-04 10:11:08 +1000

    Updated script to prepare Na wav files.

--- scripts/convert-na-wav-16kmono.sh
+++ scripts/convert-na-wav-16kmono.sh
@@ -1,6 +1,6 @@
-# 1. Download Na recordings from Lacito Pangloss.
-# 2. Convert mp3 to WAV using Audacity or something similar.
-# 3. Convert to 16k Hz
-ffmpeg -i $1 -ar 16000 16k$1
-# 4. Convert from stereo to mono
-ffmpeg -i 16k$1 -ac 1 mono16k$1
+#!/bin/bash
+
+for file in *mp3
+do
+	ffmpeg -i $file -ar 16000 -ac 1 ${file%.mp3}.wav
+done
