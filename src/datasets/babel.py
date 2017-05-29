de54cd29d72f07125f46f2b14527a9d2eb69eb44
Oliver Adams <oliver.adams@gmail.com>
2017-05-29 11:33:28 +1000

    Copying IPA transcriptions working for Turkish.

--- src/datasets/babel.py
+++ src/datasets/babel.py
@@ -31,10 +31,11 @@ LANG_DIR_MAP = {"turkish":("105-B/BABEL_BP_105", "105-turkish"),
 LANGS = list(LANG_DIR_MAP.keys())
 
 def prepare(langs=LANGS, feat_type="log_mel_filterbank"):
+    langs.remove("assamese")
     babel_sph2wav(langs)
     babelipa_transcriptions(langs)
     split_wavs_and_txts(langs)
-    feat_extraction(feat_type)
+    feat_extraction(langs, feat_type)
 
 def sph2wav(input_fn, output_fn):
     """ Call sph2pipe to convert alaw .sph files to ulaw .wav files, which will
@@ -49,6 +50,7 @@ def babel_sph2wav(langs=LANGS):
     """
 
     for lang in langs:
+        print("babel_sph2wav %s" % lang)
         org_lang_dir = os.path.join(ORG_BABEL_DIR, LANG_DIR_MAP[lang][0])
         with cd(org_lang_dir):
             for input_dir, _, input_fns in os.walk("."):
@@ -70,19 +72,23 @@ def babelipa_transcriptions(langs=LANGS):
     # training data.
 
     for lang in langs:
+        print("babelipa_transcriptions %s" % lang)
         org_lang_dir = os.path.join(BABELIPA_DIR, LANG_DIR_MAP[lang][1])
         with cd(org_lang_dir):
             for input_dir, leaf_dirs, input_fns in os.walk("."):
+#                print(input_dir, leaf_dirs, input_fns)
                 for leaf_dir in leaf_dirs:
                     if leaf_dir == "ipatranscription":
+#                        for input_fn in input_fns:
+#                        print(input_dir)
                         input_path = os.path.join(input_dir, leaf_dir)
-                        print(input_path)
+                        print("Input path:", input_path)
                         output_path = os.path.join(WORK_BABEL_DIR,
                                                    LANG_DIR_MAP[lang][1],
                                                    "conversational",
-                                                   input_dir,
-                                                   leaf_dir)
-                        print(output_path)
+                                                   input_path)
+#                                                   leaf_dir)
+                        print("Output path:", output_path)
                         shutil.copytree(input_path, output_path)
 
 def split_wavs_and_txts(langs=LANGS):
@@ -134,12 +140,11 @@ def split_wavs_and_txts(langs=LANGS):
                 pre, ext = os.path.splitext(output_txt_path)
                 output_txt_path = pre + "_utter%s" % i + ext
                 utils.make_parent(output_txt_path)
-                print(output_txt_path)
-                print(output_wav_path)
                 with open(output_txt_path, "w") as output_txt_f:
                     print(utter_txt, end="", file=output_txt_f)
 
     for lang in langs:
+        print("Splitting wavs for %s" % lang)
         org_lang_dir = os.path.join(WORK_BABEL_DIR, LANG_DIR_MAP[lang][1])
         output_dir = os.path.join(WORK_BABEL_DIR, "utters",
                                   LANG_DIR_MAP[lang][1])
@@ -149,12 +154,10 @@ def split_wavs_and_txts(langs=LANGS):
                     if input_fn.endswith(".txt"):
                         input_txt_path = os.path.join(input_dir, input_fn)
                         input_wav_path = get_wav_path(input_txt_path)
-                        print(input_txt_path)
-                        print(input_wav_path)
                         split_wav_and_txt(input_wav_path, input_txt_path)
 
 
-def feat_extraction(feat_type):
+def feat_extraction(langs, feat_type):
     """ Extracts features from all the utterances. """
 
     if feat_type != "log_mel_filterbank":
@@ -162,12 +165,16 @@ def feat_extraction(feat_type):
 
     count = 0
     utters_dir = os.path.join(WORK_BABEL_DIR, "utters")
-    for input_dir, _, input_fns in os.walk(utters_dir):
-        for input_fn in input_fns:
-            if input_fn.endswith(".wav"):
-                count += 1
-                input_path = os.path.join(input_dir, input_fn)
-                feat_extract.logfbank_feature_extraction(input_path)
+    for lang in langs:
+        print("Feature extraction for %s" % lang)
+        lang_dir = os.path.join(utters_dir, LANG_DIR_MAP[lang][1])
+        for input_dir, _, input_fns in os.walk(lang_dir):
+            for input_fn in input_fns:
+                if input_fn.endswith(".wav"):
+                    count += 1
+                    input_path = os.path.join(input_dir, input_fn)
+                    print(input_path)
+#                    feat_extract.logfbank_feature_extraction(input_path)
 
 def load_phns(lang):
     """ Loads the phone lexicon for the given language."""
@@ -180,6 +187,23 @@ def load_phns(lang):
             phones.append(line.split()[0])
 
     return phones
+
+def get_txt_path(feat_path):
+    """ Given a transcription path, gets the corresponding wav."""
+
+    pre, ext = os.path.splitext(os.path.basename(feat_path))
+    parent_dir = os.path.dirname(os.path.dirname(feat_path))
+    txt_path = os.path.join(parent_dir, "ipatranscription", "%s.txt" % pre)
+    return txt_path
+
+def filter_by_size(feat_paths, max_samples):
+    new_feat_paths = []
+    for feat_path in feat_paths:
+        _, batch_x_lens = utils.load_batch_x([feat_path], flatten=True)
+        if batch_x_lens[0] <= max_samples:
+            new_feat_paths.append(feat_path)
+    return new_feat_paths
+
 
 class Corpus(corpus.AbstractCorpus):
 
@@ -194,7 +218,7 @@ class Corpus(corpus.AbstractCorpus):
         eval_utters = []
         train_utters = []
 
-        txt_paths = []
+        feat_paths = []
         # Create list of utterance_ids based on the languages, 
         for lang in langs:
             lang_dir = os.path.join(WORK_BABEL_DIR, "utters",
@@ -206,11 +230,16 @@ class Corpus(corpus.AbstractCorpus):
             # Load transcription paths
             for input_dir, _, input_fns in os.walk(lang_dir):
                 for input_fn in input_fns:
-                    if input_fn.endswith(".txt"):
-                        txt_path = os.path.join(input_dir, input_fn)
-                        txt_paths.append(txt_path)
+                    if input_fn.endswith(".log_mel_filterbank.npy"):
+                        feat_path = os.path.join(input_dir, input_fn)
+                        feat_paths.append(feat_path)
 
-        feat_paths = [get_feat_path(txt_path) for txt_path in txt_paths]
+        if max_samples:
+            feat_paths = filter_by_size(feat_paths, max_samples)
+
+        print(len(feat_paths))
+
+        txt_paths = [get_txt_path(feat_path) for feat_path in feat_paths]
         train_feat_paths = [feat_path for feat_path in feat_paths
                             if "/training/" in feat_path]
         train_txt_paths = [txt_path for txt_path in txt_paths
@@ -220,4 +249,17 @@ class Corpus(corpus.AbstractCorpus):
         dev_txt_paths = [txt_path for txt_path in txt_paths
                          if "/dev/" in txt_path]
 
-        phones = load_phns(lang)
+        self.phonemes = load_phns(lang)
+
+        self.PHONEME_TO_INDEX = {phn: index for index, phn in enumerate(
+                                 ["pad"] + sorted(list(self.phonemes)))}
+        self.INDEX_TO_PHONEME = {index: phn for index, phn in enumerate(
+                                 ["pad"] + sorted(list(self.phonemes)))}
+        self.vocab_size = len(self.phonemes)
+
+    def get_valid_fns(self):
+        return dev_feat_paths, dev_txt_paths
+
+    def get_train_fns(self):
+        return train_feat_paths, train_txt_paths
+
