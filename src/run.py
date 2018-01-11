a19959b14de2a7c19c5c9264916aecaeb7589b2b
Oliver Adams <oliver.adams@gmail.com>
2018-01-11 11:33:16 +1100

    Batch size 64 again.

--- src/run.py
+++ src/run.py
@@ -64,9 +64,9 @@ def run():
     #train("na", "fbank_and_pitch", "phonemes_and_tones", 3, 500,
     #      train_rec_type="text")
     train("na", "fbank_and_pitch", "phonemes_and_tones", 3, 400,
-          train_rec_type="text_and_wordlist", batch_size=32)
+          train_rec_type="text_and_wordlist")
     train("na", "fbank_and_pitch", "phonemes_and_tones", 3, 500,
-          train_rec_type="text_and_wordlist", batch_size=32)
+          train_rec_type="text_and_wordlist")
 
 def multi_train():
     #train("na", "fbank", "phonemes_and_tones", 3, 250,
