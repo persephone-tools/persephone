fb6071747b6b276d9e6dcf93f532b9b3ed109b92
Oliver Adams <oliver.adams@gmail.com>
2018-01-17 17:53:11 +1100

    Added a note about tiers.

--- src/datasets/kunwinjku_steven.py
+++ src/datasets/kunwinjku_steven.py
@@ -4,6 +4,8 @@ import glob
 import os
 from os.path import join
 
+import pympi
+
 import config
 
 ORG_DIR = config.KUNWINJKU_STEVEN_DIR
@@ -31,3 +33,46 @@ def good_elan_paths():
                     elan_paths.append(elan_path)
 
     return elan_paths
+
+def explore_elan_files(elan_paths):
+    """
+    A function to explore the tiers of ELAN files.
+    """
+
+    for elan_path in elan_paths:
+        print(elan_path)
+        eafob = pympi.Elan.Eaf(elan_path)
+        tier_names = eafob.get_tier_names()
+        for tier in tier_names:
+            print("\t", tier)
+            try:
+                for annotation in eafob.get_annotation_data_for_tier(tier):
+                    print("\t\t", annotation)
+            except KeyError:
+                continue
+
+        input()
+
+"""
+rf
+rf@RN
+xv (Esther_1.eaf, but has some English in it)
+PRN_free, PRN_Pfx, NmCl_Gen, ng_DROP (Esther_2.eaf. Code switching might be an
+	issue. Otherwise Esther files are promising for the finer granularity that
+	is additionally included.)
+xv@RN (Kabo.eaf, but its a duplicate of the xv tier. More pure in kunboy.eaf)
+xv@MN, LEX (manbarndarr.eaf)
+xv@JN, PRN_pfx, PRFX (Manbedgje.eaf)
+Other (Manbulu.eaf)
+nt@JN (Mandeb.eaf, but also has English in Mandjarduk_b.eaf)
+xv@EN (mandjimdjim.eaf)
+nt@RN (20161013_manmorlak.eaf)
+rf@MARK, xv@MARK (Mark on rock. Has a few different tiers, some of which have
+multiple text fields. A bit of code switching).
+xv@GN (Marys_Yirlinkirrkirr.eaf)
+A bunch more xv@s (Njanjma_Injalak_weaving.eaf)
+Topic Index (Terrah_ngalwarrngurru.eaf)
+
+Can just check across all these tiers and any that are empty or contain empty
+strings I can just ignore.
+"""
