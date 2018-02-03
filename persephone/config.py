e2c17d8b04809929aed020c01992cfa19d3fe6a0
Janis Lesinskis <janis@customprogrammingsolutions.com>
2018-02-03 14:42:32 +1100

    Change path construction

--- persephone/config.py
+++ persephone/config.py
@@ -1,11 +1,16 @@
 """ Some variables that may be system dependent. """
+import os
+PERSONAL_CORPORA_BASE_DIR = "/lt/work/oadams/"
 
 # The directory of the original source corpora, un-preprocessed.
+# Shared across research group
 TIMIT_ORG_DIR = "/lt/data/timit/timit/"
-GRIKO_DIR = "/lt/work/oadams/griko-data/"
-CHATINO_DIR = "/lt/work/oadams/chatino/CTP/"
-JAPHUG_DIR = "/lt/work/oadams/japhug/"
-NA_DIR = "/lt/work/oadams/Na/"
+# Personal corpora files
+GRIKO_DIR = os.path.join(PERSONAL_CORPORA_BASE_DIR, "griko-data")
+CHATINO_DIR = os.path.join(PERSONAL_CORPORA_BASE_DIR, "chatino", "CTP")
+JAPHUG_DIR = os.path.join(PERSONAL_CORPORA_BASE_DIR, "japhug")
+NA_DIR = os.path.join(PERSONAL_CORPORA_BASE_DIR, "Na")
+
 # The directory where the preprocessed data will be held.
 TGT_DIR = "./data"
 # The path for experiments
