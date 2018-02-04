f1fa730cc6c49d249d1b9dfdac5a25a0c5d66bfc
Janis <janis@jaggedverge.com>
2018-02-04 23:03:25 +1100

    Created base exception class

---
+++ persephone/exceptions.py
@@ -0,0 +1,6 @@
+"""Exceptions that can be raised by the Persephone library are defined here"""
+
+class PersephoneException(Exception):
+    """Base class for all exceptions raised by the Persephone library"""
+
+
