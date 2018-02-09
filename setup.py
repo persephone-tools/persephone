2cc4a16d9e64a9422ac4eae0aee4a9006d431469
Oliver Adams <oliver.adams@gmail.com>
2018-02-09 21:07:26 +1100

    v0.1.7 version bump: bug fixes.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='persephone',
-      version='0.1.6',
+      version='0.1.7',
       description='A tool for developing automatic phoneme transcription models',
       long_description=open('README.md').read(),
       url='https://github.com/oadams/persephone',
