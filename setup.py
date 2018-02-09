86b6e78d882d56c2a3a281d41fee1eeda76c147e
Oliver Adams <oliver.adams@gmail.com>
2018-02-09 11:16:39 +1100

    Bumped version numbers.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='persephone',
-      version='0.1.5',
+      version='0.1.6',
       description='A tool for developing automatic phoneme transcription models',
       long_description=open('README.md').read(),
       url='https://github.com/oadams/persephone',
