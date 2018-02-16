0f46380e7e130f2190c98e8ef4aa539bbc909609
Oliver Adams <oliver.adams@gmail.com>
2018-02-17 08:37:03 +1100

    config.py changes.

--- persephone/config.py
+++ persephone/config.py
@@ -19,13 +19,12 @@ CORPORA_BASE_PATH = config_file.get("PATHS", "CORPORA_BASE_PATH",
                                     fallback="/lt/work/oadams/")
 
 # The directory of the original source corpora, un-preprocessed.
-# Shared across research group
-TIMIT_ORG_DIR = "/lt/data/timit/timit/"
-# Personal corpora files
-GRIKO_DIR = os.path.join(CORPORA_BASE_PATH, "griko-data")
 CHATINO_DIR = os.path.join(CORPORA_BASE_PATH, "chatino", "CTP")
-JAPHUG_DIR = os.path.join(CORPORA_BASE_PATH, "japhug")
 NA_DIR = os.path.join(CORPORA_BASE_PATH, "Na")
+# Personal corpora files
+# TODO: fix these stubs
+KUNWINJKU_STEVEN_DIR = config_file.get("PATHS", "KUNWINJKU_STEVEN_PATH", fallback="")
+EN_WORDS_PATH = config_file.get("PATHS", "EN_WORDS_PATH", fallback="")
 BUTCHER_DIR = os.path.join(CORPORA_BASE_PATH, "OA_Data")
 
 # The directory where the preprocessed data will be held.
@@ -33,13 +32,10 @@ TGT_DIR = config_file.get("PATHS", "TARGET", fallback="./data")
 # The path for experiments
 EXP_DIR = config_file.get("PATHS", "EXPERIMENTS", fallback="./exp")
 
-# The path to the sox tool for converting NIST format to WAVs.
-
+# The path to the sox tool; currently used for splitting WAVs, but we can
+# replace with pydub TODO
 SOX_PATH = config_file.get("PATHS", "SOX_BASE_PATH", fallback="/home/oadams/tools/sox-14.4.2/src/sox")
-OPENFST_BIN_PATH = config_file.get("PATHS", "OPEN_FST_BIN_PATH", fallback="/home/oadams/tools/openfst-1.6.2/src/bin")
+# FFMPEG is used for normalizing WAVs
 FFMPEG_PATH = config_file.get("PATHS", "FFMPEG_PATH", fallback="ffmpeg")
+# Kaldi is used for pitch extraction
 KALDI_ROOT = config_file.get("PATHS", "KALDI_ROOT_PATH", fallback="/home/oadams/tools/kaldi")
-
-# TODO: fix these stubs
-KUNWINJKU_STEVEN_DIR = config_file.get("PATHS", "KUNWINJKU_STEVEN_PATH", fallback="")
-EN_WORDS_PATH = config_file.get("PATHS", "EN_WORDS_PATH", fallback="")
