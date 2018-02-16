7b081fdcb5b12bbc2276f60c2110781b6eced806
Oliver Adams <oliver.adams@gmail.com>
2018-02-16 17:17:47 +1100

    deleted some test stubs.

--- persephone/tests/test_datasets.py
+++ persephone/tests/test_datasets.py
@@ -3,9 +3,6 @@ import pytest
 def test_na():
     import persephone.datasets.na
 
-def test_babel():
-    import persephone.datasets.babel
-
 def test_butcher():
     import persephone.datasets.butcher
 
@@ -13,19 +10,9 @@ def test_butcher():
 def test_kunwinjku_steven():
     import persephone.datasets.kunwinjku_steven
 
-#@pytest.mark.skip(reason="Currently this is not functional. Needs fixes before it van be imported.")
+@pytest.mark.skip(reason="Currently this is not functional. Needs fixes before it van be imported.")
 def test_chatino():
     import persephone.datasets.chatino
 
-def test_griko():
-    import persephone.datasets.griko
-
 def test_pangloss():
     import persephone.datasets.pangloss
-
-@pytest.mark.skip(reason="Currently this is not functional. Needs fixes before it van be imported.")
-def test_timit():
-    import persephone.datasets.timit
-
-def test_japhu():
-    import persephone.datasets.japhug
