ac2b90d10073791cf08f0308664815704a8a4314
Oliver Adams <oliver.adams@gmail.com>
2018-02-25 16:27:14 +1100

    Added a fixture to collect data for BKW testing.

---
+++ persephone/tests/test_bkw.py
@@ -0,0 +1,32 @@
+""" Testing Persephone on Alex/Steven's Kunwinjku data. """
+
+from pathlib import Path
+import subprocess
+
+import pytest
+
+from persephone import config
+
+@pytest.fixture
+def prepare_bkw_data():
+    # Ensure the BKW data is all there
+    bkw_path = Path(config.BKW_PATH)
+    if not bkw_path.is_dir():
+        raise NotImplementedError(
+            "Data isn't available and I haven't figured out how authentication"
+            " should best work for datasets that aren't yet public.")
+    assert bkw_path.is_dir()
+
+    # Ensure english-words/words.txt is there.
+    ENGLISH_WORDS_URL = "https://github.com/dwyl/english-words.git"
+    en_words_path = Path(config.EN_WORDS_PATH)
+    if not en_words_path.is_file():
+        subprocess.run(["git", "clone",
+                        ENGLISH_WORDS_URL, str(en_words_path.parent)])
+    assert en_words_path.is_file()
+
+    return en_words_path
+
+def test_en_words(prepare_bkw_data):
+    with prepare_bkw_data.open() as f:
+        print(f.read())
