a1c966dfa802e2180a1adddc4449f515c700bb1b
Oliver Adams <oliver.adams@gmail.com>
2018-03-02 09:01:09 +1100

    Added pytest.mark.experiment marker for experiments.

--- conftest.py
+++ conftest.py
@@ -1,14 +1,20 @@
-"""Configuration for unit test suite, allows you to automatically
-skip tests marked as slow unless you provide the --runslow option
+"""Configuration for automated test suite, allows you to automatically
+skip tests marked as slow unless you provide the --slow option
 on the commandline when invoking pytest
 """
 
 import pytest
 
 def pytest_addoption(parser):
-    parser.addoption("--runslow", action="store_true", help="run slow tests")
+    parser.addoption("--slow", action="store_true", help="run slow tests")
     parser.addoption("--notravis", action="store_true",
-        help="run tests that travis doesn't run by default")
+        help="Run tests that travis doesn't run by default. These tests "\
+             "typically require substantial data that would need to be "\
+             "fetched from elsewhere.")
+    parser.addoption("--experiment", action="store_true",
+        help="Runs end-to-end experiments. This is for reproducing reported "\
+             "results and to ensure models converge with larger amounts of "\
+             "data.")
 
 def pytest_runtest_setup(item):
     """
@@ -19,8 +25,16 @@ def pytest_runtest_setup(item):
     require data unavailable to Travis. This includes datasets that aren't open
     or would be a bit time consuming to download even though they are available
     locally.
+
+    The 'experiment' marker is for experiments where error rate results need to
+    demonstrate adequate convergence. They are typically long-running, and the
+    whole suite would take days or weeks to run if all the experiments were
+    done together.
     """
-    if 'slow' in item.keywords and not item.config.getoption("--runslow"):
-        pytest.skip("need --runslow option to run")
+
+    if 'slow' in item.keywords and not item.config.getoption("--slow"):
+        pytest.skip("need --slow option to run")
     if 'notravis' in item.keywords and not item.config.getoption("--notravis"):
         pytest.skip("need --notravis option to run")
+    if 'experiment' in item.keywords and not item.config.getoption("--experiment"):
+        pytest.skip("need --experiment option to run")
