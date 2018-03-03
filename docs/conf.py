2eb6b9920e10afbf451eff93181c012cfb83be11
Oliver Adams <oliver.adams@gmail.com>
2018-03-03 14:13:05 +1100

    Test remove sphinx_autodoc_typehints. Trying again.

--- docs/conf.py
+++ docs/conf.py
@@ -46,7 +46,7 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
     'sphinx.ext.napoleon',
-    'sphinx_autodoc_typehints',
+#    'sphinx_autodoc_typehints',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
