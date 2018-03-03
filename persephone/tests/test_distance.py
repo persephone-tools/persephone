e35d210ded327d454bf56660f9d8931fdc53d944
Oliver Adams <oliver.adams@gmail.com>
2018-03-03 21:31:52 +1100

    Randomized the substitution cost too.

--- persephone/tests/test_distance.py
+++ persephone/tests/test_distance.py
@@ -12,10 +12,12 @@ def rand_str(length: int, alphabet: str = string.digits) -> str:
 
 @pytest.fixture
 def seq_cases():
-    hardcoded_seqs = [("", "", 0),
-                      ("abde", "abcde", 1),
-                      ([1,3,5], [], 3),
-                      ([1,3,5], [3], 2),
+    """ Cases are of the form (reference, hypothesis, substitution_cost, dist).
+    """
+    hardcoded_seqs = [("", "", 1, 0),
+                      ("abde", "abcde", 1, 1),
+                      ([1,3,5], [], 1, 3),
+                      ([1,3,5], [3], 1, 2),
                      ]
 
     # Here we assume the nltk.metrics.distance implementation is correct.
@@ -25,16 +27,20 @@ def seq_cases():
             length2 = random.randint(0, int(length*1.5))
             s1 = rand_str(length)
             s2 = rand_str(length2)
-            dist = distance.edit_distance(s1, s2)
-            generated_seqs.append((s1, s2, dist))
+            sub_cost = random.randint(0, 3)
+            dist = distance.edit_distance(s1, s2, substitution_cost=sub_cost)
+            generated_seqs.append((s1, s2, sub_cost, dist))
 
     return hardcoded_seqs + generated_seqs
 
 def test_med1(seq_cases):
 
-    for s1, s2, dist in seq_cases:
-        assert min_edit_distance(s1, s2) == dist
-        print(s1, s2)
+    # Tests edit distance implementation against that of NLTK, with randomized
+    # strings and substitution cost functions.
+    for s1, s2, sub_cost, dist in seq_cases:
+        print(s1, s2, sub_cost, dist)
+        sub_cost_f = lambda x, y: 0 if x == y else sub_cost
+        assert min_edit_distance(s1, s2, sub_cost=sub_cost_f) == dist
         if len(s1) == 0:
             with pytest.raises(EmptyReferenceException):
                 word_error_rate(s1, s2)
