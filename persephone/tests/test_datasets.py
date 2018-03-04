37323e9a4de1cab2f4043d22fd92c3403ee04d48
Oliver Adams <oliver.adams@gmail.com>
2018-03-04 18:44:10 +1100

    Some testing changes.

--- persephone/tests/test_datasets.py
+++ persephone/tests/test_datasets.py
@@ -3,10 +3,5 @@ import pytest
 def test_na():
     import persephone.datasets.na
 
-@pytest.mark.skip(reason="Currently this is not functional. Needs fixes before it van be imported.")
 def test_kunwinjku_steven():
-    import persephone.datasets.kunwinjku_steven
-
-@pytest.mark.skip(reason="Currently this is not functional. Needs fixes before it van be imported.")
-def test_chatino():
-    import persephone.datasets.chatino
+    import persephone.datasets.bkw
