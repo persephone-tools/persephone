1d90cfdfc32ec842862226de05c38dc30666c9ed
Oliver Adams <oliver.adams@gmail.com>
2018-03-05 07:47:51 +1100

    Got speaker-specific validation stats for Mark.

--- persephone/tests/test_results.py
+++ persephone/tests/test_results.py
@@ -1,8 +1,10 @@
+from collections import defaultdict
 from pathlib import Path
 
 import pytest
 
 from persephone import results
+from persephone import utils
 from persephone.corpus import Corpus
 
 #@pytest.fixture
@@ -11,7 +13,7 @@ from persephone.corpus import Corpus
 
 def test_speaker_results():
     data_path = Path("testing/data/bkw")
-    exp_path = Path("testing/exp/20/")
+    exp_path = Path("testing/exp/19/")
     # TODO I shouldn't really be using "decoded" for the validation set dir
     # anymore.
     hyps_path = exp_path / "decoded" / "best_hyps"
@@ -24,14 +26,30 @@ def test_speaker_results():
 
     corp = Corpus.from_pickle(data_path)
 
+    import pprint
+
+    speaker = "Mark Djandiomerr"
+    speaker_prefixes = defaultdict(list)
+    assert corp.utterances
+    for utter in corp.utterances:
+        if utter.speaker == speaker and utter.prefix in corp.valid_prefixes:
+            speaker_prefixes[speaker].append(utter.prefix)
+
+    pprint.pprint(speaker_prefixes)
+
+    speaker_hyps_refs = defaultdict(list)
+    prefix_hyps_refs = zip(corp.valid_prefixes, hyps, refs)
+    for prefix, hyp, ref in prefix_hyps_refs:
+        if prefix in speaker_prefixes[speaker]:
+            speaker_hyps_refs[speaker].append((hyp, ref))
+    print(len(speaker_hyps_refs[speaker]))
+    print(utils.batch_per(*zip(*speaker_hyps_refs[speaker])))
+
+    return
+
     # Create a Dict[speaker, List[prefix]]
 
     # Create a Dict[speaker, Tuple[List[hyp], List[ref]]]
-
-    from persephone import distance
-    from persephone import utils
-
-    from collections import defaultdict
 
     def make_spk2hypsrefs(hyps, refs, prefixes, prefix2speaker):
         spk2hypsrefs = defaultdict(lambda: [[],[]])
