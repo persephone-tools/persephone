e37383f5b0773302e12113d0bdf930b11f072864
Janis <janis@jaggedverge.com>
2018-03-27 02:28:53 +1100

    Attach a logger to sys.excepthook to log any unhandled exceptions

--- persephone/__init__.py
+++ persephone/__init__.py
@@ -1,1 +1,17 @@
 __version__ = "0.2.0"
+
+import sys
+import logging
+
+logging.config.fileConfig(config.LOGGING_INI_PATH)
+logger = logging.getLogger(__name__)
+
+def handle_unhandled_exception(exc_type, exc_value, exc_traceback):
+    """Handler for unhandled exceptions that will write to the logs"""
+    if issubclass(exc_type, KeyboardInterrupt):
+        # call the default excepthook saved at __excepthook__
+        sys.__excepthook__(exc_type, exc_value, exc_traceback)
+        return
+    logger.critical("Unhandled exception", exc_info=(exc_type, exc_value, exc_traceback))
+
+sys.excepthook = handle_unhandled_exception
