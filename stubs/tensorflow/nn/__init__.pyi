ae6f9de9ea97cc04925aa165a85c38c13b9092f6
Janis <janis@jaggedverge.com>
2018-05-25 21:19:40 +1000

    Implement type stub for ctc_beam_search_decoder

--- stubs/tensorflow/nn/__init__.pyi
+++ stubs/tensorflow/nn/__init__.pyi
@@ -1,5 +1,10 @@
 from typing import Any, Optional, Tuple
 
+# ctc_beam_search_decoder implemented here:
+# https://github.com/tensorflow/tensorflow/blob/bb4e724f429ae5c9afad3a343dc1f483ecde1f74/tensorflow/python/ops/ctc_ops.py#L234
+def ctc_beam_search_decoder(inputs : Any , sequence_length: Any, beam_width: int =100,
+                            top_paths: int = 1, merge_repeated: bool = True) -> Tuple[Any, Any]: ...
+
 # bidirectional_dynamic_rnn implemented here:
 # https://github.com/tensorflow/tensorflow/blob/d8f9538ab48e3c677aaf532769d29bc29a05b76e/tensorflow/python/ops/rnn.py#L314
 # TODO: types
