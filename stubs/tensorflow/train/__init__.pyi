6234b8e13a8e5487a863cf5d2dd0e6ca0e9ac8c3
Janis <janis@jaggedverge.com>
2018-05-25 20:06:41 +1000

    Create mypy type stub for Session.save

--- stubs/tensorflow/train/__init__.pyi
+++ stubs/tensorflow/train/__init__.pyi
@@ -1,8 +1,17 @@
-from typing import Any
+from typing import Any, Optional
 
 def import_meta_graph(path: str) -> Any:
     pass
 
+# Saver class defined here
+# https://github.com/tensorflow/tensorflow/blob/28340a4b12e286fe14bb7ac08aebe325c3e150b4/tensorflow/python/training/saver.py#L1075
 class Saver:
     def restore(self, session: Any, path: str) -> None:
         pass
+
+    # TODO: parameter types:
+    #    sess is of type Session
+    #    global_step is of type Tensor or integer
+    def save(self, sess: Any, save_path: str, global_step: Any = None, latest_filename: Optional[str]=None,
+             meta_graph_suffix: str = "meta", write_meta_graph: bool = True, write_state: bool = True,
+             strip_default_attrs: bool = False) -> Optional[str]: ...
