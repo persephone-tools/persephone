d5f5eb487f20ad7503c2be8b20d0a6483f1cfcc1
Janis <janis@jaggedverge.com>
2018-05-25 19:19:09 +1000

    Added mypy type stub for Tensorflow BaseSession to cover Session.run

--- stubs/tensorflow/__init__.pyi
+++ stubs/tensorflow/__init__.pyi
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, Dict
 
 from . import train
 
@@ -14,7 +14,18 @@ class ConfigProto:
 class Graph:
     pass
 
-class Session:
+class BaseSession:
+    #TODO: options is of type RunOption, run_metadata is of type RunMetadata
+    # Return type is option of:
+    # single graph element if fetches is a single graph element  OR
+    # list of graph elements if fetches is a list of single graph elements OR
+    # a dictionary
+    # Leaving it as Any for now
+    def run(self, fetches: Any, feed_dict: Dict[str, Any] = None, run_options: Any = None, run_metadata: Any = None) -> Any: ...
+
+    def close(self) -> None: ...
+
+class Session(BaseSession):
     def __init__(self, graph: Graph = None, config: ConfigProto = None) -> None:
         pass
     def __enter__(self):
