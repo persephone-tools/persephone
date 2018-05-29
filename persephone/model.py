e8aa549190128a050c9454c0a83a1324c11a3bd9
Janis <janis@jaggedverge.com>
2018-05-29 17:38:52 +1000

    idiomatic comparisons

--- persephone/model.py
+++ persephone/model.py
@@ -318,7 +318,7 @@ class Model:
                     epoch_str = "Epoch %d. Training LER: %f, validation LER: %f, validation PER: %f" % (
                         epoch, (train_ler_total / (batch_i + 1)), valid_ler, valid_per)
                     print(epoch_str, flush=True, file=out_file)
-                    if best_epoch_str == None:
+                    if best_epoch_str is None:
                         best_epoch_str = epoch_str
 
                     # Implement early stopping.
