66b0df245a316ea239586a015ef674ca793fb554
Janis <janis@jaggedverge.com>
2018-06-01 01:27:32 +1000

    add test case for missing directory

--- persephone/tests/test_corpus.py
+++ persephone/tests/test_corpus.py
@@ -7,3 +7,12 @@ def test_ready_corpus_deprecation():
             ReadyCorpus(tgt_dir="test_dir")
         except FileNotFoundError:
             pass
+
+
+def test_determine_labels_throws():
+    """Test that a non existant directory will throw"""
+    import pathlib
+    from persephone.corpus import determine_labels
+    non_existent_path = "thispathdoesntexist"
+    with pytest.raises(FileNotFoundError):
+        determine_labels(non_existent_path, "phonemes")
