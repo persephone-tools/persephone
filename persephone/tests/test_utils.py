90171c735e909f28c22c37255d3d9eaa9f23e109
Janis <janis@customprogrammingsolutions.com>
2018-08-20 02:12:25 +1000

    Create unit test for utils.make_batches

--- persephone/tests/test_utils.py
+++ persephone/tests/test_utils.py
@@ -4,3 +4,17 @@ def test_is_number():
     assert is_number("12")
     assert is_number("12.0")
     assert not is_number("This isn't a number")
+
+
+def test_make_batches():
+    """Test that the batch generator creates the right batches"""
+    from persephone.utils import make_batches
+    paths = [1,2,3,4]
+
+    batches1 = make_batches(paths, 1)
+    assert len(batches1) == 4
+    assert batches1 == [[1],[2],[3],[4]]
+
+    batches2 = make_batches(paths, 2)
+    assert len(batches2) == 2
+    assert batches2 == [[1,2],[3,4]]
