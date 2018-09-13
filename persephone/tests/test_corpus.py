f911bb0f071d51179d15b2b7d4a8bf20dbe081d1
Janis <janis@customprogrammingsolutions.com>
2018-09-13 15:57:29 +1000

    Fix typo

--- persephone/tests/test_corpus.py
+++ persephone/tests/test_corpus.py
@@ -120,7 +120,7 @@ def test_corpus_with_predefined_data_sets(tmpdir, create_sine, make_wav):
 
     test_prefixes = tmpdir.join("test_prefixes.txt").write("a")
     train_prefixes = tmpdir.join("train_prefixes.txt").write("b")
-    valid_prefixes = tmpdir.join("vaild_prefixes.txt").write("c")
+    valid_prefixes = tmpdir.join("valid_prefixes.txt").write("c")
 
     c = Corpus(
         feat_type='fbank',
