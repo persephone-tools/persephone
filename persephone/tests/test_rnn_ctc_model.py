22816296db28ee74113e28a2204ce8e54a2fb408
Janis <janis@customprogrammingsolutions.com>
2018-09-15 03:20:17 +1000

    Fix up path to checkpoint save file

--- persephone/tests/test_rnn_ctc_model.py
+++ persephone/tests/test_rnn_ctc_model.py
@@ -18,20 +18,20 @@ def test_model_creation(create_test_corpus):
     )
     assert model
 
-def test_model_train_and_decode(tmpdir, create_test_corpus):
+def test_model_train_and_decode(tmpdir, create_sine, make_wav, create_test_corpus):
     """Test that we can create a model, train it then decode something with it"""
     from persephone.corpus_reader import CorpusReader
     from persephone.rnn_ctc import Model
+    from pathlib import Path
     corpus = create_test_corpus()
 
-    # If it turns out that `tgt_dir` is not in the public interface this test should change
-    # to get the base directory from the fixture that created it.
+    # If it turns out that `tgt_dir` is not in the public interface of the Corpus
+    # this test should change and get the base directory from the fixture that created it.
     base_directory = corpus.tgt_dir
     print("base_directory", base_directory)
 
     corpus_r = CorpusReader(
         corpus,
-        num_train=1,
         batch_size=1
     )
     assert corpus_r
@@ -39,8 +39,8 @@ def test_model_train_and_decode(tmpdir, create_test_corpus):
     test_model = Model(
         base_directory,
         corpus_r,
-        num_layers=2,
-        hidden_size=10
+        num_layers=3,
+        hidden_size=100
     )
     assert test_model
 
@@ -52,16 +52,20 @@ def test_model_train_and_decode(tmpdir, create_test_corpus):
 
     from persephone.model import decode
 
-    wav_to_decode_path = Path(tmpdir.join("to_decide.wav"))
+    wav_to_decode_path = str(tmpdir.join("to_decode.wav"))
     sine_to_decode = create_sine(note="C")
 
     make_wav(sine_to_decode, wav_to_decode_path)
 
+    output_path = tmpdir.mkdir("decode_output")
+
+    model_checkpoint_path = base_directory / "model" / "model_best.ckpt"
     result = decode(
-        base_directory,
-        [wav_to_decode_path],
-        label_set = {"a", "b", "c"},
+        model_checkpoint_path,
+        [Path(wav_to_decode_path)],
+        label_set = {"A", "B", "C"},
         feature_type = "fbank",
+        preprocessed_output_path=Path(str(output_path)),
         batch_x_name = test_model.batch_x,
         batch_x_lens_name = test_model.batch_x_lens,
         output_name = test_model.outputs
