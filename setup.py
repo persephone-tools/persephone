b4ce92fc9c3e574bd3f769d374e8261b1378858f
Janis <janis@customprogrammingsolutions.com>
2019-08-31 18:07:48 +1000

    Add Python 3.7 classifier to metadata

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(name='persephone',
           'Programming Language :: Python',
           'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
       ],
       keywords='speech-recognition machine-learning acoustic-models artificial-intelligence neural-networks',
       install_requires=[
